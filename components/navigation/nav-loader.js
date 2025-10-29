fc2a1e7568593a6467901661ebd08d8ea34c2d89
Hamilton Ko <hammisaur@gmail.com>
2025-10-28 23:45:20 -0700

    Created template files for navigation bar

---
+++ components/navigation/nav-loader.js
@@ -0,0 +1,21 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navContainer = document.getElementById("nav");
+    if (!navContainer) {
+        return;
+    }
+
+    const navFolder = '/components/navigation/';
+
+    fetch(navFolder + 'navigation.html')
+        .then(response => response.text())
+        .then(data => {
+            navContainer.innerHTML = data;
+            if (!document.querySelector('link[href="${navFolder}navigation.css"]')) {
+                let link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = navFolder + 'navigation.css'
+                document.head.appendChild(link);
+            }
+        })
+});
