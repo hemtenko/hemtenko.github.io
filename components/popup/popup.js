b2c73775639cf7d94861ac64884d117a710bb85f
Hamilton Ko <hammisaur@gmail.com>
2025-11-03 00:34:02 -0800

    Adjust how file popup works/behaves

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -1,35 +1,38 @@
 const container = document.getElementById("popup-container");
 
-container.innerHTML = 
-` 
-<div id="window" class="popup">
+container.innerHTML =
+  `<div id="window" class="popup">
     <div class="popup-content">
       <span class="close">&times;</span>
       <iframe id="frame" src="" frameborder="0"></iframe>
     </div>
-  </div>
-`;
+  </div>`;
 
 const modal = document.getElementById("window");
 const frame = document.getElementById("frame");
 const close = document.querySelector(".close");
 
-  document.querySelectorAll("a.doc-link").forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-      frame.src = this.href;
-      modal.style.display = "flex";
-    });
+document.querySelectorAll("a.doc-link").forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    frame.src = this.href;
+    modal.style.display = "flex";
+
+    document.body.style.overflow = "hidden";
   });
+});
 
-  close.onclick = function() {
-    modal.style.display = "none";
-    frame.src = "";
+function closePopup() {
+  modal.style.display = "none"; 
+  frame.src = "";
+  
+  document.body.style.overflow = "auto";
+}
+
+close.onclick = closePopup;
+
+window.onclick = function (e) {
+  if (e.target === modal) {
+    closePopup();
   }
-
-  window.onclick = function(e) {
-    if (e.target === modal) {
-      modal.style.display = "none";
-      frame.src = "";
-    }
-  };
+};
