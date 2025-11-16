0d96c190323c98837dfca611971c57829588b1ef
Hamilton Ko <hammisaur@gmail.com>
2025-11-16 00:34:53 -0800

    Updated styling and coding for repeating elements

--- components/navigation/nav-loader.js
+++ components/navigation/nav-loader.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const navContainer = document.getElementById("nav");
     if (!navContainer) {
--- components/popup/popup.js
+++ components/popup/popup.js
@@ -1,38 +1,50 @@
-const container = document.getElementById("popup-container");
-
-container.innerHTML =
-  `<div id="window" class="popup">
-    <div class="popup-content">
-      <span class="close">&times;</span>
-      <iframe id="frame" src="" frameborder="0"></iframe>
-    </div>
-  </div>`;
-
-const modal = document.getElementById("window");
-const frame = document.getElementById("frame");
-const close = document.querySelector(".close");
-
-document.querySelectorAll("a.doc-link").forEach(link => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    frame.src = this.href;
-    modal.style.display = "flex";
-
-    document.body.style.overflow = "hidden";
-  });
-});
-
-function closePopup() {
-  modal.style.display = "none"; 
-  frame.src = "";
-  
-  document.body.style.overflow = "auto";
-}
-
-close.onclick = closePopup;
-
-window.onclick = function (e) {
-  if (e.target === modal) {
-    closePopup();
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.getElementById("popup-container");
+  if (!container) {
+    return;
   }
-};
+
+  const popupFolder = '/components/popup/';
+
+  fetch(popupFolder + "popup.html")
+    .then(response => response.text())
+    .then(data => {
+      container.innerHTML = data;
+
+      if (!document.querySelector('link[href="${popupFolder}popup.css"]')) {
+        let link = document.createElement('link');
+        link.rel = "stylesheet";
+        link.href = popupFolder + 'popup.css';
+        document.head.appendChild(link);
+      }
+
+      const modal = document.getElementById("window");
+      const frame = document.getElementById("frame");
+      const close = document.querySelector(".close");
+
+      document.querySelectorAll("a.doc-link").forEach(link => {
+        link.addEventListener("click", function (e) {
+          e.preventDefault();
+          frame.src = this.href;
+          modal.style.display = "flex";
+
+          document.body.style.overflow = "hidden";
+        });
+      });
+
+      function closePopup() {
+        modal.style.display = "none";
+        frame.src = "";
+
+        document.body.style.overflow = "auto";
+      }
+
+      close.onclick = closePopup;
+
+      window.onclick = function (e) {
+        if (e.target === modal) {
+          closePopup();
+        }
+      };
+    })
+});
