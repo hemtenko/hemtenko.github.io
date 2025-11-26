bfd34e63b5a845bd291e4f443dbc1ba949edbfdf
Hamilton Ko <hammisaur@gmail.com>
2025-11-25 23:53:13 -0800

    Updated how navigation bar works on mobile

--- components/navigation/nav-loader.js
+++ components/navigation/nav-loader.js
@@ -1,20 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const navContainer = document.getElementById("nav");
-    if (!navContainer) {
-        return;
-    }
+    document.addEventListener('DOMContentLoaded', function () {
+        const navContainer = document.getElementById("nav");
+        if (!navContainer) {
+            return;
+        }
 
-    const navFolder = '/components/navigation/';
+        const navFolder = '/components/navigation/';
 
-    fetch(navFolder + 'navigation.html')
-        .then(response => response.text())
-        .then(data => {
-            navContainer.innerHTML = data;
-            if (!document.querySelector('link[href="${navFolder}navigation.css"]')) {
-                let link = document.createElement('link');
-                link.rel = 'stylesheet';
-                link.href = navFolder + 'navigation.css'
-                document.head.appendChild(link);
-            }
-        })
-});
+        fetch(navFolder + 'navigation.html')
+            .then(response => response.text())
+            .then(data => {
+                navContainer.innerHTML = data;
+                if (!document.querySelector('link[href="${navFolder}navigation.css"]')) {
+                    let link = document.createElement('link');
+                    link.rel = 'stylesheet';
+                    link.href = navFolder + 'navigation.css'
+                    document.head.appendChild(link);
+                }
+
+                const toggle = document.getElementById("menu-toggle");
+                const navigation = navContainer.querySelector(".navigation");
+                const dropdown = navContainer.querySelector('.dropdown');
+
+                if (toggle && navigation) {
+                    toggle.addEventListener('click', function() {
+                        navigation.classList.toggle('open');
+
+                        if (window.innerWidth <= 768) {
+                            dropdown.classList.add("open");
+                        }
+                    });
+                }
+            });
+    });
